95f0c62ccf739f85c7df5a258e42ec54fb54dc4d
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-06-23 07:52:09 +0100

    Tag01: Kompilierfehler, Laufzeitfehler Tag02: Mathe-Operationen, Typenkonvertierung

---
+++ src/Tag01.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    // Beispiel Kompilierfehler:
+    //var x: String = 13
+
+    // Beispiel Laufzeitfehler
+    var list = listOf(1,2,3,4)
+    println(list[5])
+
+    // println() und readln()
+    println("Bitte dein Alter eingeben.")
+    var alter: String = readln()
+    println("Okay, du bist also $alter Jahre alt!")
+
+}
---
+++ src/Tag02.kt
@@ -0,0 +1,17 @@
+fun main(){
+    // Variablen und Datentypen
+
+    // Integer: ganze Zahlen ohne Nachkommastellen
+    var intNumber: Int  = 13
+    val DAYS_OF_THE_WEEK: Int = 7
+
+    // Double: Zahlen mit Nachkommastellen
+    var doubleNumber: Double = 5.0
+    val PI: Double = 3.14
+
+
+    // Boolean: Wahrheitswert, immer entweder true oder false
+    var isSunShining: Boolean = false
+    val IS_EARTH_TURNING: Boolean = true
+    
+}
