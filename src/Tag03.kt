106ae770a2e55a5f735c923d602a9ec3a3a11fde
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-06-23 07:53:52 +0100

    Tag03: Mathe-Operationen, Typenkonvertierung

---
+++ src/Tag03.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    // Mathematische Operationen:
+    var intSum = 30+70
+    var doubleDiff = 10.5-7.5
+    var product = 8*8
+    var divident = 15.0/3.0
+    var rest = 90%20
+
+    // Type Conversion
+    var stringValue = readln()
+    var convertedString = stringValue.toDouble()
+
+    var doubleValue = convertedString
+    var convertedDouble = convertedString.toInt()
+
+
+}
